e257bdab9d82ee92959a83c64a3b04447c6a884a
Paulinebh <pauline@bodin-hullin.net>
2012-12-11 23:30:35 +0100

    Update index.php
    Remplacement ! par .

--- index.php
+++ index.php
@@ -65,7 +65,7 @@ $portfolioSize = round(filesize(__DIR__ . $portfolioPath) / 1024 / 1024, 2) . 'M
             </ul>
 
             <div id="info">
-                <p id="open">&gt; <strong>Actuellement en recherche d'emploi !</strong></p>
+                <p id="open">&gt; <strong>Actuellement en recherche d'emploi.</strong></p>
 
                 <ul id="links">
                     <li class="contact"><a href="#contact">Me contacter</a></li>
